dedce22b3a706337ca670fd3eb3bf79da0b8fdc3
unknown <daudakolo16@gmail.com>
2023-04-16 22:44:53 +0100

    Debug

--- src/utils/rootUrl.ts
+++ src/utils/rootUrl.ts
@@ -1,3 +1,3 @@
-export const APIURL = "http://127.0.0.1:8000";
+export const APIURL = "https://tapi-dendo.herokuapp.com";
 export const APIKEY =
   "16c176befc7c6615b88569eafcc45e42519e2b8097a5437e2fb7513264c4557deb1a75";
